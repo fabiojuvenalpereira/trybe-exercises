9920181763f153484d6add719e54611c7245b8d5
fabio <fabiojuvenalpereira@hotmail.com>
2021-05-19 21:52:28 -0300

    adiciona exercício-2

---
+++ exercicio-2/retorna-maior-numero.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 7;
+
+if (a > b) {
+    console.log('O maior número é o' + a);
+} else if (b > a) {
+    console.log('O maior número é o:' + b);
+} else {
+    console.log('Os números são iguais.');
+}

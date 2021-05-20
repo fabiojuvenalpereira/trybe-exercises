2809fde0e7f435c10436c4f662f11c920a3bae63
fabio <fabiojuvenalpereira@hotmail.com>
2021-05-19 23:25:09 -0300

    Alteração e adição do exercício-8

---
+++ exercicio-8/definindo-se-e-par.js
@@ -0,0 +1,9 @@
+let numero1 = 1;
+let numero2 = 13;
+let numero3 = 15;
+
+if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0) {
+    console.log('O número é par');
+} else {
+    console.log('O número é impar');
+}

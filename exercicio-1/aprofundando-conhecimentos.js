c4b07816b273c95f2a5bccecc9413a34c194ea91
fabio <fabiojuvenalpereira@hotmail.com>
2021-05-20 19:07:29 -0300

    adiona parte 2

--- exercicio-1/aprofundando-conhecimentos.js
+++ exercicio-1/aprofundando-conhecimentos.js
@@ -1,6 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-    let mostraNumero = numbers[index];
-    console.log(mostraNumero)
+    let showNumber = numbers[index];
+    sum = sum + showNumber;
 }
+console.log('A soma do Array é: ' + sum);
+// exercicio 3

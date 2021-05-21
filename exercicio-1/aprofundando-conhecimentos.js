925283bd17de9044303c6797ac6de13e4a0f9944
fabio <fabiojuvenalpereira@hotmail.com>
2021-05-21 17:44:03 -0300

    Adiciona exercicio-6

--- exercicio-1/aprofundando-conhecimentos.js
+++ exercicio-1/aprofundando-conhecimentos.js
@@ -1,25 +1,28 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
-
-// for (let index = 0; index < numbers.length; index += 1) {
-//     let showNumber = numbers[index];
-//     sum = sum + showNumber;
-// }
-// console.log('A soma do Array é: ' + sum);
-
-// // exercicio 3
-// let med = sum / numbers.length;
-// console.log('A média é: ' + med);
-
-// // exercicio 4
-// if (med > 20) {
-//     console.log('O valor é maior que 20');
-// } else {
-//     console.log('O valor é menor ou igual a 20');
-// }
-
-// exercicio 5
+let contadorimpar = 0;
+let contadorpar = 0;
 let maiorValor = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    let showNumber = numbers[index];
+    sum = sum + showNumber;
+}
+console.log('A soma do Array é: ' + sum);
+
+// exercicio 3
+let med = sum / numbers.length;
+console.log('A média é: ' + med);
+
+// exercicio 4
+if (med > 20) {
+    console.log('O valor é maior que 20');
+} else {
+    console.log('O valor é menor ou igual a 20');
+}
+
+//exercicio 5
+
 
 for (let indexCompare = 0; indexCompare < numbers.length; indexCompare += 1) {
 
@@ -27,4 +30,17 @@ for (let indexCompare = 0; indexCompare < numbers.length; indexCompare += 1) {
         maiorValor = numbers[indexCompare];
     }
 }
-console.log(maiorValor);
+console.log('O maior número do Array é: ' + maiorValor);
+
+//exercicio 6
+
+for (let percorre = 0; percorre < numbers.length; percorre += 1) {
+    if (numbers[percorre] % 2 === 1) {
+        contadorimpar += 1;
+    }
+}
+if (contadorimpar === 0) {
+    console.log('Nenhum valor encontrado')
+} else {
+    console.log('São ' + contadorimpar + ' Números Ímpares.');
+}

bfc0f34ea1cfcf65ae24e0801db507e124f933dc
fabio <fabiojuvenalpereira@hotmail.com>
2021-05-21 19:17:04 -0300

    exercicio 1

---
+++ exercises-1/quadrado.js
@@ -0,0 +1,9 @@
+let n = 10;
+let asteristico = '';
+
+for (let indexX = 0; indexX <= n; indexX += 1) {
+    asteristico += '*';
+}
+for (let indexY = 0; indexY < n; indexY += 1) {
+    console.log(asteristico)
+}

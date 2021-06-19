5692faf86cf88856f93e70ab18cad205776a622e
fabio <fabiojuvenalpereira@hotmail.com>
2021-06-18 22:38:57 -0300

    Adiciona Parte 2 do exercicio 1

--- exercicios_pt1_1.js
+++ exercicios_pt1_1.js
@@ -36,13 +36,18 @@ const order = {
 
 const customerInfo = (order) => {
   console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
-
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
+  const change = order.name = 'João'; 
+  const flavor = Object.keys(order.order.pizza);
+  const getDrink = order.order.drinks.coke.type;
 
+  const price = order.payment.total = '50,00';
+  // console.log(price);
+  console.log(`Olá ${change}, o total do seu pedido de ${flavor[0]},${flavor[1]} e ${getDrink} é de R$${price}`);
 }
 
 orderModifier(order);

fe0811871169705aab450b68acb73db42a4a441c
fabio <fabiojuvenalpereira@hotmail.com>
2021-07-17 11:24:15 -0300

    parte 2

---
+++ dia-1/techList.js
@@ -0,0 +1,13 @@
+function techList(arr,name) {
+  if (arr.length === 0 | !name)  return 'Vazio!'
+    const listT = arr.sort().map((item) => {
+      return  {
+        tech: item,
+        name: name,
+      }
+    });
+  return listT;
+}
+
+module.exports = techList;
+
